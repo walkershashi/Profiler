e7a97154e14efe717977dd5c652efd376ea5b52c
shashiwalker <skssunny30@gmail.com>
2020-08-05 09:42:23 +0530

    Modified the personal details header and added the DOB and EMAIL subheaders.

--- resume_generater.py
+++ resume_generater.py
@@ -40,7 +40,7 @@ def list_table(list_):
     table_data = []
     
     for ind, sk in enumerate(list_):
-        table_data.append([ind, sk])
+        table_data.append([ind + 1, sk])
     
     skill_table = Table(
         data = table_data,
@@ -54,10 +54,14 @@ def generate_resume(details):
 
     styles = getSampleStyleSheet()
     
-    name, dob, age = details['Personal Details'].values()
-    report_title = Paragraph('Personal Details', styles['h1'])
-    report_subtitle = Paragraph(name, styles['h3'])
+    name, dob, email = details['Personal Details'].values()
     
+    detail_title = Paragraph('Personal Details', styles['h1'])
+    detail_table = Table(
+        data = [[key, val] for key, val in details['Personal Details'].items()],
+        hAlign = 'CENTER'
+    )
+
     edu_title = Paragraph('<br/>Educational Details<br/>', styles['h1'])
     edu_table = build_table(details, 'Educational Qualifications')
     
@@ -82,4 +86,9 @@ def generate_resume(details):
     hobi_title = Paragraph('<br/>Hobbies & Interests', styles['h1'])
     hobi_table = list_table(details['Hobbies & Interests'])
 
-    report.build([report_title, report_subtitle, edu_title, edu_table, work_title, work_table, skills_title, skills_table, project_title, project_table, training_title, training_table, volunteer_title, volunteer_table, acad_title, acad_table, hobi_title, hobi_table])
+    report.build([detail_title, detail_table, edu_title, edu_table, work_title, work_table, skills_title, skills_table, project_title, project_table, training_title, training_table, volunteer_title, volunteer_table, acad_title, acad_table, hobi_title, hobi_table])
+
+with open('candidate_details.json', 'r') as json_file:
+    json_data = json.load(json_file)
+
+generate_resume(json_data)
